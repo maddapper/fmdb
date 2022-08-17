63c280a0af32136fffc0189e38badae3c1b0f5d8
Dean Chang <dean.chang@freestar.io>
2022-08-17 15:51:24 -0700

    SPM dynamic library flag.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "FMDB",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "FMDB", targets: ["FMDB"]),
+        .library(name: "FMDB", type: .dynamic, targets: ["FMDB"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
